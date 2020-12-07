b1bbca9feab9742195f1e06e1e8695fedf15b22d
Manveersinghchug <72390809+Manveersinghchug@users.noreply.github.com>
2020-12-07 17:03:34 +0530

    Add files via upload

---
+++ Virtual-Pet--2-master/sketch.js
@@ -0,0 +1,30 @@
+var dog,milk;
+
+function preload()
+{
+  dog = loadImage("sprites/download.jpg");
+  milk = loadImage("sprites/milk.jpg");
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+  dog = createSprite(200,300,20,20);
+  dog.addImage(download.jpg);                
+  milk = createSprite(200,300,20,20);
+  milk.addImage(milk.jpg);                
+
+
+
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
